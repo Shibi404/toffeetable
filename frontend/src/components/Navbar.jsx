a798b4ecc87e33bc9ecaeb3fc67fcd237abf54be
Shibi404 <shibisbs123@gmail.com>
2026-01-27 19:30:14 +0530

    ToffeeTable: Add Submenu for Menu in Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,12 +2,14 @@ import { useState, useEffect } from "react";
 import logo from "../assets/images/Logo Transparent.png";
 
 function Navbar() {
-  const [open, setOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false); // Mobile Hamburger
+  const [isSubOpen, setIsSubOpen] = useState(false); // Menu Dropdown
 
   useEffect(() => {
     const handleClick = (e) => {
       if (!e.target.closest("#navbar")) {
-        setOpen(false);
+        setIsOpen(false);
+        setIsSubOpen(false);
       }
     };
     document.addEventListener("click", handleClick);
@@ -15,49 +17,54 @@ function Navbar() {
   }, []);
 
   return (
-      <nav id="navbar">
-        <div id="nav-mob-icon">
-          {!open ? (
-            <i
-              className="fa-solid fa-bars"
-              style={{ color: "#5c4033", fontSize: "20px" }}
-              onClick={() => setOpen(true)}
-            ></i>
-          ) : (
-            <i
-              className="fa-solid fa-xmark"
-              style={{ color: "#5c4033", fontSize: "23px" }}
-              onClick={() => setOpen(false)}
-            ></i>
-          )}
-        </div>
+    <nav id="navbar">
+      {/* 1. Hamburger Icon */}
+      <div id="nav-mob-icon" onClick={() => setIsOpen(!isOpen)}>
+        <i className={`fa-solid ${isOpen ? "fa-xmark" : "fa-bars"}`}></i>
+      </div>
 
-        <div id="logo">
-          <img src={logo} alt="Toffee Table Logo" />
-        </div>
+      {/* 2. Logo */}
+      <div id="logo">
+        <img src={logo} alt="Toffee Table Logo" />
+      </div>
 
-        <div
-          id="nav-links"
-          style={{ maxHeight: open ? "280px" : "0px" }}
-        >
-          <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/">Menu</a></li>
-            <li><a href="/">Gallery</a></li>
-            <li><a href="/">Contact</a></li>
-          </ul>
-        </div>
+      {/* 3. Links Container - MUST HAVE THE CLASS "open" */}
+      <div id="nav-links" className={isOpen ? "open" : ""}>
+        <ul className="nav-main-links">
+          <li><a href="/">Home</a></li>
 
-        <div id="nav-icons">
-          <ul>
-            <li>
-              <a href="/">
-                <i className="fa-solid fa-cart-shopping" style={{ color: "#5c4033" }}></i>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+          {/* Submenu Trigger - MUST HAVE THE CLASS "active" */}
+          <li
+            className={`menu-item ${isSubOpen ? "active" : ""}`}
+            onClick={(e) => {
+              if (window.innerWidth <= 768) {
+                e.stopPropagation();
+                setIsSubOpen(!isSubOpen);
+              }
+            }}
+          >
+            <span className="menu-title">
+              Menu <i className="fa-solid fa-chevron-down"></i>
+            </span>
+
+            <ul className="dropdown">
+              <li><a href="/menu/cakes">Cakes</a></li>
+              <li><a href="/menu/brownies">Brownies</a></li>
+              <li><a href="/menu/cupcakes">Cupcakes</a></li>
+              <li><a href="/menu/pastries">Pastries</a></li>
+            </ul>
+          </li>
+
+          <li><a href="/">Gallery</a></li>
+          <li><a href="/">Contact</a></li>
+        </ul>
+      </div>
+
+      {/* 4. Cart */}
+      <div id="nav-icons">
+        <i className="fa-solid fa-cart-shopping"></i>
+      </div>
+    </nav>
   );
 }
 
