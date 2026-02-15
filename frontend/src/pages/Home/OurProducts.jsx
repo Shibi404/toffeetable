bbb05ce65fbd5116ddce46ad25e08f7027a8318a
Shibi404 <shibisbs123@gmail.com>
2026-02-15 15:49:27 +0530

    ToffeeTable: Add Link to Products in Homepage

--- frontend/src/pages/Home/OurProducts.jsx
+++ frontend/src/pages/Home/OurProducts.jsx
@@ -1,28 +1,30 @@
+import { Link } from "react-router-dom";
 import cakes from "../../assets/images/cakes.webp";
 import brownies from "../../assets/images/brownies.webp";
 import cupcakes from "../../assets/images/cupcakes.webp";
 import pastries from "../../assets/images/pastries.webp";
 
 const products = [
-  { img: cakes, name: "Cakes" },
-  { img: brownies, name: "Brownies" },
-  { img: cupcakes, name: "Cupcakes" },
-  { img: pastries, name: "Pastries" },
+  { img: cakes, name: "Cakes", path: "/menu/cakes" },
+  { img: brownies, name: "Brownies", path: "/menu/brownies" },
+  { img: cupcakes, name: "Cupcakes", path: "/menu/cupcakes" },
+  { img: pastries, name: "Pastries", path: "/menu/pastries" },
 ];
 
 function OurProducts() {
   return (
     <section id="our-products">
       <section className="site-container">
-      <h3 className="heading">OUR PRODUCTS</h3>
-      <div className="product-container">
-        {products.map((item, index) => (
-          <div className="product-element" key={index}>
-            <img src={item.img} alt={item.name} />
-            <h4>{item.name}</h4>
-          </div>
-        ))}
-      </div>
+        <h3 className="heading">OUR PRODUCTS</h3>
+
+        <div className="product-container">
+          {products.map((item, index) => (
+            <Link to={item.path} className="product-element" key={index}>
+              <img src={item.img} alt={item.name} />
+              <h4>{item.name}</h4>
+            </Link>
+          ))}
+        </div>
       </section>
     </section>
   );
